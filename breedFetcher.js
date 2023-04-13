92f6c20ed62cfe62a56ae3932571d8867d987b83
abeljlim <subrosian@hotmail.com>
2023-04-13 16:45:50 -0700

    implementation of edge cases and printing a description and eslint on breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,23 @@
 const request = require('request');
 const args = process.argv.slice(2);
 
-const srcURLEndpoint = `https://api.thecatapi.com/v1/breeds/search`;
+const srcURLEndpoint = `https://api.thgresecatapi.com/v1/breeds/search`;
 const srcURL = `${srcURLEndpoint}?q=${args[0]}`;
 
-const downloader = function (sourceUrl, destPath) {
+const downloader = function(sourceUrl) {
   request(sourceUrl, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
 
-    // console.log(typeof body);
     const data = JSON.parse(body);
-    console.log(data[0]);
-    // console.log(typeof data[0]);
+    if (data.length === 0) {
+      console.log("Error: Breed not found");
+      return;
+    }
+    console.log(data[0].description);
 
   });
 };
-downloader(srcURL, ``);
+downloader(srcURL);
