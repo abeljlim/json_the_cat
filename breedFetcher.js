06857b00a0839f29b1f9b2d2519545492b3c8083
abeljlim <subrosian@hotmail.com>
2023-04-13 17:12:09 -0700

    Refactored breedFetcher.js and made breedFetcher.js export a function to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,23 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
 const srcURLEndpoint = `https://api.thgresecatapi.com/v1/breeds/search`;
-const srcURL = `${srcURLEndpoint}?q=${args[0]}`;
 
-const downloader = function(sourceUrl) {
-  request(sourceUrl, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  const srcURL = `${srcURLEndpoint}?q=${breedName}`;
+  request(srcURL, (error, response, body) => {
     if (error) {
-      console.log(error);
+      callback(error, null);
       return;
     }
-
     const data = JSON.parse(body);
     if (data.length === 0) {
       console.log("Error: Breed not found");
       return;
     }
-    console.log(data[0].description);
 
+    // happy path
+    callback(null, data[0].description);
   });
 };
-downloader(srcURL);
+
+module.exports = { fetchBreedDescription };
